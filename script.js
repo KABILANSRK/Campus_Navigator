5a1675da54806849b9375a415bf4b1cde3749feb
Kabilan_SRK <kabilansrk@gmail.com>
2024-03-29 14:06:27 +0530

    We've completed the prototype for now. We'll circle back to integrate it into Blender at a later stage.

---
+++ script.js
@@ -0,0 +1,10 @@
+// script.js
+
+// Example JavaScript code to display a message in the console
+console.log("");
+
+// Example JavaScript code to handle a button click event
+function handleClick() {
+  alert("Button clicked!");
+}
+
