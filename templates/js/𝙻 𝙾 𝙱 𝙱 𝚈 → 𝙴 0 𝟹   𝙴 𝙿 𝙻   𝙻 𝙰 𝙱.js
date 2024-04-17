0e20e7f982cafdd039d713b8e10984fe4b980c5e
Kabilan_SRK <kabilansrk@gmail.com>
2024-04-17 16:00:18 +0530

    organized code

---
+++ templates/js/𝙻 𝙾 𝙱 𝙱 𝚈 → 𝙴 0 𝟹   𝙴 𝙿 𝙻   𝙻 𝙰 𝙱.js
@@ -0,0 +1,14 @@
+// Function to redirect to the index.html page
+function redirectToIndex() {
+    window.location.href = "index.html";
+}
+
+// Show/hide scroll to top button based on scroll position
+window.onscroll = function() {
+    var scrollToTopBtn = document.getElementById("scroll-to-top");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollToTopBtn.style.display = "block";
+    } else {
+        scrollToTopBtn.style.display = "none";
+    }
+};
